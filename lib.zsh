e523a1c50e311aef6126c359d6bd28fe1a634b07
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2019-12-08 10:58:00 +0900

    Remove err output of a command to check gnu coreutils

--- lib.zsh
+++ lib.zsh
@@ -22,7 +22,7 @@ alias has='__exists_command'
 __is_gnu_coreutils() {
   local -r CMD=${1}
 
-  ${CMD} --version | grep -q GNU
+  ${CMD} --version 2>&1 | grep -q GNU
 }
 
 alias gnu='__is_gnu_coreutils'
